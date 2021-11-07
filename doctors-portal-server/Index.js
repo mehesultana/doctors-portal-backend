1e500112ff0b867742558f1e1f8fed060e65e3ba
mehesultana <sultanamehe01@gmail.com>
2021-11-07 19:43:10 +0600

    Display User specific Appointments using Data Table

--- doctors-portal-server/Index.js
+++ doctors-portal-server/Index.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4chv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -15,7 +16,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
 	try {
 		await client.connect();
-		console.log('database connected successfully');
+		// console.log('database connected successfully');
+
+		const database = client.db('doctors_portal');
+		const appointmentsCollection = database.collection('appointments');
+
+		app.get('/appointments', async (req, res) => {
+			const email = req.query.email;
+			const date = new Date(req.query.date).toLocaleDateString();
+
+			const query = { email: email, date: date };
+
+			const cursor = appointmentsCollection.find(query);
+			const appointments = await cursor.toArray();
+			res.json(appointments);
+		});
+
+		app.post('/appointments', async (req, res) => {
+			const appointment = req.body;
+			const result = await appointmentsCollection.insertOne(appointment);
+			console.log(result);
+			res.json(result);
+		});
 	} finally {
 		// await client.close();
 	}
